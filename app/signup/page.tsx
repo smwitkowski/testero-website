3a0f7c19a973bb08ac7f6c239eead7a8ae924d4b
Stephen Witkowski <stephen.witkowski@protonmail.com>
2025-06-10 09:37:00 -0400

    Refactor signup form fields

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -26,6 +26,59 @@ function getSignupErrorCode(message: string): string {
   return 'unknown_error';
 }
 
+function getInputClass(hasError: boolean, isDirty: boolean): string {
+  const base =
+    'px-4 py-3 text-base rounded-md transition-all duration-300 border-2';
+  if (hasError) {
+    return `${base} border-red-400 bg-red-50`;
+  }
+  if (isDirty) {
+    return `${base} border-green-400 bg-green-50`;
+  }
+  return `${base} border-slate-300 focus:border-orange-400 focus:ring focus:ring-orange-200 focus:ring-opacity-50`;
+}
+
+function FormFieldIcons({
+  hasError,
+  isDirty,
+}: {
+  hasError: boolean;
+  isDirty: boolean;
+}) {
+  if (!isDirty) return null;
+  return (
+    <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+      {hasError ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 text-red-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 text-green-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+            clipRule="evenodd"
+          />
+        </svg>
+      )}
+    </div>
+  );
+}
+
 const signupFormSchema = z.object({
   email: z
     .string()
@@ -118,13 +171,7 @@ const SignupPage = () => {
                                 <Input
                                   type="email"
                                   placeholder="Email address"
-                                  className={`px-4 py-3 text-base rounded-md transition-all duration-300 border-2 ${
-                                    fieldState.error
-                                      ? 'border-red-400 bg-red-50'
-                                      : fieldState.isDirty && !fieldState.error
-                                        ? 'border-green-400 bg-green-50'
-                                        : 'border-slate-300 focus:border-orange-400 focus:ring focus:ring-orange-200 focus:ring-opacity-50'
-                                  }`}
+                                  className={getInputClass(!!fieldState.error, fieldState.isDirty)}
                                   disabled={isSubmitting}
                                   autoComplete="email"
                                   autoFocus
@@ -133,19 +180,7 @@ const SignupPage = () => {
                                   {...field}
                                 />
                               </FormControl>
-                              {fieldState.isDirty && (
-                                <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
-                                  {fieldState.error ? (
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-                                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-                                    </svg>
-                                  ) : (
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
-                                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                                    </svg>
-                                  )}
-                                </div>
-                              )}
+                              <FormFieldIcons hasError={!!fieldState.error} isDirty={fieldState.isDirty} />
                             </div>
                             <FormMessage className="text-left mt-1 font-medium" />
                           </FormItem>
@@ -162,13 +197,7 @@ const SignupPage = () => {
                                 <Input
                                   type="password"
                                   placeholder="Password"
-                                  className={`px-4 py-3 text-base rounded-md transition-all duration-300 border-2 ${
-                                    fieldState.error
-                                      ? 'border-red-400 bg-red-50'
-                                      : fieldState.isDirty && !fieldState.error
-                                        ? 'border-green-400 bg-green-50'
-                                        : 'border-slate-300 focus:border-orange-400 focus:ring focus:ring-orange-200 focus:ring-opacity-50'
-                                  }`}
+                                  className={getInputClass(!!fieldState.error, fieldState.isDirty)}
                                   disabled={isSubmitting}
                                   autoComplete="new-password"
                                   aria-required="true"
@@ -176,19 +205,7 @@ const SignupPage = () => {
                                   {...field}
                                 />
                               </FormControl>
-                              {fieldState.isDirty && (
-                                <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
-                                  {fieldState.error ? (
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-                                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-                                    </svg>
-                                  ) : (
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor">
-                                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                                    </svg>
-                                  )}
-                                </div>
-                              )}
+                              <FormFieldIcons hasError={!!fieldState.error} isDirty={fieldState.isDirty} />
                             </div>
                             <FormMessage className="text-left mt-1 font-medium" />
                           </FormItem>
